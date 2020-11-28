31f9f2c0cabdb4df69709a95675d7731eabf0333
ziadsherif3 <ziadsherif3@gmail.com>
2020-11-29 01:04:36 +0200

    Added the Post model

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # date_posted = models.DateTimeField(auto_now=true) # The date is updated every time the post changes
+    # date_posted = models.DateTimeField(auto_now_add=true) # The date is updated only when the post is created
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {'author': 'Ziad Sherif',
-     'title': 'Post 1',
-     'content': 'First post content',
-     'date_posted': 'November 27, 2020'
-    },
-    {'author': 'John Doe',
-     'title': 'Post 2',
-     'content': 'Second post content',
-     'date_posted': 'November 26, 2020'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     
     return render(request, 'blog/home.html', context)
