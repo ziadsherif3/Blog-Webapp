9130ab5b8f2fd3fa72dde8a5756c70d8474c87b8
ziadsherif3 <ziadsherif3@gmail.com>
2020-12-23 21:52:51 +0200

    When a user registers a profile is created for the user

--- blog_website/blog_website/settings.py
+++ blog_website/blog_website/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
