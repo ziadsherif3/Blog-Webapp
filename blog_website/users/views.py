ef9cc4be6c356c8041570116c12b9a944fafb597
ziadsherif3 <ziadsherif3@gmail.com>
2020-12-25 22:34:04 +0200

    Users can update their profile information and upload an image

--- blog_website/users/views.py
+++ blog_website/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from blog.models import Post
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -23,8 +23,25 @@ def register(request):
 @login_required
 def profile(request):
     current_user = request.user
-    context = {
-        'posts': current_user.post_set.all()
-    }
     
-    return render(request, 'users/profile.html', context)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=current_user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=current_user.profile)
+        
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Profile updated successfully!')
+            return redirect('profile')
+        
+    else:
+        u_form = UserUpdateForm(instance=current_user)
+        p_form = ProfileUpdateForm(instance=current_user.profile)
+    
+        context = {
+            'posts': current_user.post_set.all(),
+            'u_form': u_form,
+            'p_form': p_form
+        }
+        
+        return render(request, 'users/profile.html', context)
