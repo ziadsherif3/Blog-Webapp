00c268d4825c33cb73b75c2461977eef80507eae
ziadsherif3 <ziadsherif3@gmail.com>
2020-12-27 00:20:27 +0200

    Added the ability to view, create, update, or delete a post

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse
 from .models import Post
 
 # Create your views here.
@@ -10,6 +13,54 @@ def home(request):
     }
     
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('blog-home')
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        
+        if post.author == self.request.user:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    
+    def test_func(self):
+        post = self.get_object()
+        
+        if post.author == self.request.user:
+            return True
+        return False
+    
+    def get_success_url(self):
+        return reverse('blog-home')
 
 def about(request):
     
--- blog_website/users/models.py
+++ blog_website/users/models.py
@@ -6,16 +6,16 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='/profile_pics/default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
     
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         
         img = Image.open(self.image.path)
-        
+
         if img.height > 300 or img.width > 300:
-            img.thumbnail((300, 300))
+            img.thumbnail((300,300))
             img.save(self.image.path)
